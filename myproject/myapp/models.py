0f2b07e8231ec1cda8f914c106f67230b907e2f0
alotav <alonsotaverna@hotmail.com>
2022-11-11 15:49:02 -0300

    completamos modulo 6 - ultimo modulo

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+# admin - educacionit
+
 # Create your models here.
 
 class Curso(models.Model):
@@ -12,8 +14,18 @@ class Curso(models.Model):
     )
     turno = models.PositiveSmallIntegerField(choices = turnos, null = True)
 
+    def __str__(self):
+        return self.nombre
+
 
 class Instructor(models.Model):
     nombre = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     cursos_asignados = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Instructor"
+        verbose_name_plural = "Instructores"
+
+    def __str__(self):
+        return self.nombre
